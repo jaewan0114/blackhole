467d645b7c2b3927d69b9f66fb8b80a393e369ed
Tomohisa Kusano <siomiz@gmail.com>
2015-08-31 23:31:55 +0000

    tested ok, really call ip route; try to handle SIGTERM right

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # "tail" based on http://stackoverflow.com/a/12523302
 # "2>/dev/null" based on http://stackoverflow.com/a/11269627
 
-import threading, Queue, subprocess, re, os
+import threading, Queue, subprocess, re, os, signal, sys
 
 tailq = Queue.Queue(maxsize=10)
 state = []
@@ -119,8 +119,7 @@ class Block(object):
         cmd = ['/usr/sbin/ip', 'route'] + cmd
         print '    calling: %s' % ' '.join(cmd)
         with open(os.devnull, 'wb') as devnull:
-            pass
-            # subprocess.call(["/usr/sbin/ip", "route"] + cmd, stderr=devnull)
+            subprocess.call(["/usr/sbin/ip", "route"] + cmd, stderr=devnull)
 
 
 def blackhole(ipaddr):
@@ -173,27 +172,40 @@ def blackhole(ipaddr):
 
 
 def tail_forever():
-    p = subprocess.Popen(
-        ["/usr/bin/journalctl", "-D", "/var/log/journal", "_COMM=sshd", "-o", "cat", "-f"],
-        stdout=subprocess.PIPE)
+    try:
+        p = subprocess.Popen(
+            ["/usr/bin/journalctl", "-D", "/var/log/journal", "_COMM=sshd", "-o", "cat", "-f"],
+            stdout=subprocess.PIPE)
+        while True:
+            line = p.stdout.readline()
+            tailq.put(line)
+            if not line:
+                break
+    finally:
+        p.terminate()
+
+
+def sigterm_handler(sig, frame):
+    sys.exit()
+
+
+signal.signal(signal.SIGTERM, sigterm_handler)
+
+th = threading.Thread(target=tail_forever)
+th.start()
+
+try:
     while True:
-        line = p.stdout.readline()
-        tailq.put(line)
-        if not line:
-            break
-
-
-threading.Thread(target=tail_forever).start()
-
-while True:
-    line = tailq.get()
-    m = re.match('Invalid user .* from ([0-9\.]*)', line) \
-        or re.match('User .* from ([0-9\.]*) not allowed', line) \
-        or re.match('Connection closed by ([0-9\.]*) \[preauth\]', line) \
-        or re.match('Received disconnect from ([0-9\.]*):.*\[preauth\]', line)
-    if m:
-        ipaddr = m.group(1)
-        print
-        print ipaddr
-        blackhole(ipaddr)
+        line = tailq.get()
+        m = re.match('Invalid user .* from ([0-9\.]*)', line) \
+            or re.match('User .* from ([0-9\.]*) not allowed', line) \
+            or re.match('Connection closed by ([0-9\.]*) \[preauth\]', line) \
+            or re.match('Received disconnect from ([0-9\.]*):.*\[preauth\]', line)
+        if m:
+            ipaddr = m.group(1)
+            print
+            print ipaddr
+            blackhole(ipaddr)
+finally:
+    th.join()
 
